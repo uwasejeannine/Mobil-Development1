8cc3d750c6e057e956d18496319b01aa9862a088
uwasejeannine <jeannineuwasee@gmail.com>
2021-02-21 00:22:13 +0300

    assignment1

---
+++ src/assignment1.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val name="jeannineUwase"
+    print(name)
+    var age=20
+    print(age)
+    var waight=53.535353
+    print(waight)
+    var phone=250785952374
+    print(phone)
+    val citizen="true"
+    print(citizen)
+    val student=arrayOf("jeannineUwase",20,53.535353,250785952374,"true")
+    print(student)
+
+
+}
